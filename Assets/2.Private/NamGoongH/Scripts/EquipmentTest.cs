a1430b3b89b66289116353f443d160de4147533c
kumachandeath <namgungha1@gmail.com>
2024-12-23 21:33:58 +0900

    [Test] 테스트용 코드 수정
    장비 내역 출력

--- Assets/2.Private/NamGoongH/Scripts/EquipmentTest.cs
+++ Assets/2.Private/NamGoongH/Scripts/EquipmentTest.cs
@@ -13,12 +13,15 @@ public class EquipmentTest : MonoBehaviour
         // 슬롯에 맞는 장비를 랜덤 생성하여 장착
         Equipment headGear = Equipment.GenerateEquipment(E_EquipmentsType.Head, 1);
         playerStats.EquipItem(headGear);
+        PrintEquipmentData(headGear);
 
         Equipment chestArmor = Equipment.GenerateEquipment(E_EquipmentsType.Chest, 1);
         playerStats.EquipItem(chestArmor);
+        PrintEquipmentData(chestArmor);
 
         Equipment boots = Equipment.GenerateEquipment(E_EquipmentsType.Boots, 1);
         playerStats.EquipItem(boots);
+        PrintEquipmentData(boots);
     }
 
     public void PrintEquipmentData(Equipment equipment)
