b0827e0a5e5975210adb7abc6a443857675d4d4d
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-12-23 17:01:45 +0900

    [Feat] 중간보스몬스터 어택 기능 구현중
    중간보스몬스터 어택 트리 조건 및 Action, Condition task 구현중

--- Assets/2.Private/LimJH/Scripts/Behavior Tree/IsCheckAttackCountS.cs
+++ Assets/2.Private/LimJH/Scripts/Behavior Tree/IsCheckAttackCountS.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
 
-public class IsCheckAttackCount : Conditional
+public class IsCheckAttackCountS : Conditional
 {
     public SharedInt attackCount;
     public SharedInt specialAttackCount;
@@ -10,6 +10,6 @@ public class IsCheckAttackCount : Conditional
     public override TaskStatus OnUpdate()
     {
         // 공격 횟수가 필요 공격 횟수 이상일 때는 특수 공격을 수행
-        return attackCount.Value < specialAttackCount.Value? TaskStatus.Success : TaskStatus.Failure;
+        return attackCount.Value >= specialAttackCount.Value? TaskStatus.Success : TaskStatus.Failure;
     }
 }
