967d22542dbdbae8eb92c44c645b495826215e99
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-12-24 21:32:06 +0900

    [Test] 중간보스 특수 공격 조건 테스트
    특수 공격 이후 공격 횟수를 -1로 초기화하며 조건을 통해 공격을 시도하면서 공격 횟수가 0으로 초기화 될 때 공격을 수행하는 부분을 수정해보려고 시도 중

--- Assets/2.Private/LimJH/Scripts/Behavior Tree/IsCheckAttackCount.cs
+++ Assets/2.Private/LimJH/Scripts/Behavior Tree/IsCheckAttackCount.cs
@@ -6,9 +6,17 @@ public class IsCheckAttackCount : Conditional
 {
     public SharedInt attackCount;
     public SharedInt specialAttackCount;
+    public SharedBool isDelay;
+
+
 
     public override TaskStatus OnUpdate()
     {
+        if (isDelay.Value)
+        {
+            return TaskStatus.Failure;
+        }
+
         // 공격 횟수가 필요 공격 횟수 이상일 때는 특수 공격을 수행
         return attackCount.Value < specialAttackCount.Value? TaskStatus.Success : TaskStatus.Failure;
     }
