a4aac1cc3ac7a00e54bc266f47d240cfefacca7d
ijhijhijh7 <43941348+ijhijhijh7@users.noreply.github.com>
2024-12-26 21:15:46 +0900

    [Fix] 몬스터 애니메이션 조건 수정중
    몬스터 애니메이션 조건 수정중

--- Assets/2.Private/LimJH/Scripts/Behavior Tree/Attack.cs
+++ Assets/2.Private/LimJH/Scripts/Behavior Tree/Attack.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using BehaviorDesigner.Runtime;
 using BehaviorDesigner.Runtime.Tasks;
+using Cysharp.Threading.Tasks;
 
 public class Attack : Action
 {
@@ -8,11 +9,14 @@ public class Attack : Action
     public SharedGameObject targetObject;
     public SharedFloat attackDamage;
     public SharedInt attackCount;
+    public SharedBool canAttack;
 
     public float attackRange;
     public float angle;
 
     private Animator animator; // Animator 컴포넌트
+
+    private BaseMonster baseMonster;
 
     public override void OnStart()
     {
@@ -22,53 +26,60 @@ public class Attack : Action
             return;
         }
 
+        if (baseMonster == null)
+        {
+            baseMonster = gameObject.GetComponent<BaseMonster>();
+        }
+
         // Animator 컴포넌트 가져오기
         if (selfObject.Value != null)
         {
+            canAttack.Value = true;
             animator = selfObject.Value.GetComponent<Animator>();
-        }
+            animator.SetBool("isAttack", true);
 
-        animator.SetBool("isAttack", true);
+            // 테스크 실행 => while 현재 애니메이션이 재생중인지 확인을해서 끝난시점을 잡고 OnEnd 내부 로직을 실행시키고 return TaskStatus.Success; 화이팅
+            AttackRoutine(baseMonster).Forget();
+        }
     }
 
-    public override TaskStatus OnUpdate()
+    /*public override TaskStatus OnUpdate()
     {
-        if (targetObject.Value == null)
-        {
-            Debug.LogWarning("타겟 오브젝트가 존재하지 않습니다.");
-            return TaskStatus.Failure;
-        }
+         if (targetObject.Value == null)
+         {
+             Debug.LogWarning("타겟 오브젝트가 존재하지 않습니다.");
+             return TaskStatus.Failure;
+         }
 
-        //대상의 baseMonster컴포넌트에 데미지 전달
-        var player = targetObject.Value.GetComponent<ProjectPlayer>();
-        if (player != null && GetAngleHit(targetObject.Value.transform))
-        {
-            if(attackCount.Value!=-1) 
-            {
-                player.TakeDamage(attackDamage.Value);
-                Debug.Log(attackDamage.Value + "의 데미지를 " + targetObject.Value.name + "에게 주었습니다.");
-                //attackCount.Value++;
-            }
-            attackCount.Value++;
-        }
-        else
-        {
-            Debug.LogWarning("컴포넌트가 존재하지 않습니다.");
-        }
+         //대상의 baseMonster컴포넌트에 데미지 전달
+         var player = targetObject.Value.GetComponent<ProjectPlayer>();
+         if (player != null *//*&& GetAngleHit(targetObject.Value.transform*//*)
+         {
+             if(attackCount.Value!=-1 && canAttack.Value) 
+             {
+                 player.TakeDamage(attackDamage.Value);
+                 Debug.Log(attackDamage.Value + "의 데미지를 " + targetObject.Value.name + "에게 주었습니다.");
+                 //attackCount.Value++;
+             }
+             attackCount.Value++;
+         }
+         else
+         {
+             Debug.LogWarning("컴포넌트가 존재하지 않습니다.");
+         }
+        return TaskStatus.Running;
+    }*/
 
-        return TaskStatus.Success;
-    }
-
-    public override void OnEnd()
+    /*public override void OnEnd()
     {
         // 공격 종료 작업
         if (animator != null)
         {
-            animator.SetBool("isMove", false);
+            animator.SetBool("isAttack", false);
         }
-    }
+    }*/
 
-    public bool GetAngleHit(Transform target)
+    /*public bool GetAngleHit(Transform target)
     {
         // 내각 및 거리 계산
         Vector3 directionToTarget = (target.position - transform.position).normalized;
@@ -85,5 +96,59 @@ public class Attack : Action
         }
 
         return false; // 타격 실패
+    }*/
+
+    /*private bool IsAttackAnimationFinished()
+    {
+        if (animator != null)
+        {
+            
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            return stateInfo.IsName("isAttack") && stateInfo.normalizedTime >= 1.0f; // normalizedTime이 1 이상이면 애니메이션 종료
+        }
+        return false;
+    }*/
+
+    //canAttack 어디서 초기화 할지
+    private async UniTask<TaskStatus> AttackRoutine(BaseMonster baseMonster)
+    {
+        if (targetObject.Value == null)
+        {
+            Debug.LogWarning("타겟 오브젝트가 존재하지 않습니다.");
+            return TaskStatus.Failure;
+        }
+
+        // 애니메이션이 종료될 때까지 대기
+        while (animator != null)
+        {
+            AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.IsName("Jake_Attack") && stateInfo.normalizedTime >= 1.0f)
+            {
+                break;
+            }
+            await UniTask.Yield();
+        }
+
+        //대상의 baseMonster컴포넌트에 데미지 전달
+        var player = targetObject.Value.GetComponent<ProjectPlayer>();
+        if (player != null /*&& GetAngleHit(targetObject.Value.transform*/)
+        {
+            if (attackCount.Value != -1 && canAttack.Value)
+            {
+                player.TakeDamage(attackDamage.Value);
+                Debug.Log(attackDamage.Value + "의 데미지를 " + targetObject.Value.name + "에게 주었습니다.");
+                //attackCount.Value++;
+            }
+            attackCount.Value++;
+        }
+        else
+        {
+            Debug.LogWarning("컴포넌트가 존재하지 않습니다.");
+        }
+
+        canAttack.Value = false;
+        animator.SetBool("isAttack", false);
+
+        return TaskStatus.Success;
     }
 }
