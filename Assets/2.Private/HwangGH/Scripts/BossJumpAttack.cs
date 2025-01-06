c5c56fe522397f590a82eae3a96635b95f4f54d2
GuGuJelly <jellypuding@naver.com>
2025-01-06 16:07:10 +0900

    [Feat] 백업

--- Assets/2.Private/HwangGH/Scripts/BossJumpAttack.cs
+++ Assets/2.Private/HwangGH/Scripts/BossJumpAttack.cs
@@ -4,17 +4,39 @@ using BehaviorDesigner.Runtime.Tasks;
 
 public class BossJumpAttack : Action
 {
-	public SharedVector3 targetPos; // 플레이어 평면위치
-	public SharedFloat height;
+	[Header("GameObject")]
+	public SharedGameObject targetObject;
 
+	[Header("Property")]
+	public Vector3 targetPos; // 플레이어 평면위치
+	public SharedFloat jumpHeight;
+	public SharedFloat elapse;
 
-	public override void OnStart()
+    public override void OnAwake()
+    {
+		targetObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void OnStart()
 	{
-		
-	}
+		if (targetObject == null)
+			return;
+
+		elapse = 0;
+        jumpHeight = 0;
+
+        targetPos = new Vector3(targetObject.Value.transform.position.x,
+                                     targetObject.Value.transform.position.y + jumpHeight.Value,
+                                     targetObject.Value.transform.position.z);
+
+        // 점프 애니메이션 트리거 추가 가능
+        Debug.Log("점프 시작!");
+    }
 
 	public override TaskStatus OnUpdate()
 	{
-		return TaskStatus.Success;
+        // jumpHeight = transform.position.y
+
+        return TaskStatus.Success;
 	}
 }
