426dc56c6e8618d72010d02edcff37661471cfbc
Z1758 <timeofwitch@gmail.com>
2025-01-16 13:46:20 +0900

    Feat : 해상도 설정

--- Assets/2.Private/KimSW/Scripts/OptionUI/ResolutionOption.cs
+++ Assets/2.Private/KimSW/Scripts/OptionUI/ResolutionOption.cs
@@ -1,13 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class ResolutionOption : MonoBehaviour, ISaveOption
 {
-    public void SaveOption()
+    [SerializeField] int[] resolutionX;
+    [SerializeField] int[] resolutionY;
+    [SerializeField] TMP_Dropdown dropdown;
+
+    int tempNum;
+    int changeNum;
+
+    private void OnEnable()
     {
-        Debug.Log("해상도 저장");
+       dropdown.value = changeNum;
     }
 
-    
+    public void SaveOption()
+    {
+        Screen.SetResolution(resolutionX[tempNum], resolutionY[tempNum], false);
+        changeNum = tempNum;
+        
+    }
+
+    public void SetResolution(TMP_Dropdown change)
+    {
+        tempNum = change.value;
+    }
+
 }
